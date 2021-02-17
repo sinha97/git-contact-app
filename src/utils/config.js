ce3b58477f23e3eda2f27e6d5bf243110b6d2712
sinha97 <vk9633698@gmail.com>
2021-02-17 19:23:53 +0530

    contact application

---
+++ src/utils/config.js
@@ -0,0 +1,17 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDhImcLhmgcNnuMESnwFe4ggrVoZRLdck0",
+  authDomain: "my-git-contact-app.firebaseapp.com",
+  databaseURL: "https://my-git-contact-app-default-rtdb.firebaseio.com",
+  projectId: "my-git-contact-app",
+  storageBucket: "my-git-contact-app.appspot.com",
+  messagingSenderId: "206896614427",
+  appId: "1:206896614427:web:acdf55073b65e8eaf77cbe"
+};
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};;
